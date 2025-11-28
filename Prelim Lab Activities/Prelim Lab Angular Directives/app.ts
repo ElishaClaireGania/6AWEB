6e47985a7c100a8c88f1f310bb38a10d0df124af
Elisha Claire A. Gania <elishaclairegania@gmail.com>
2025-11-28 17:24:32 +0800

    Add files via upload

---
+++ Prelim Lab Activities/Prelim Lab Angular Directives/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Directives } from './directives/directives';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Directives],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('directives');
+}
